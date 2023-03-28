a99b7cb4221b92a3ce611cb80c42c16716ace22e
Shadowlight001 <taifjihane533@gmail.com>
2023-03-28 02:13:07 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
+
+
