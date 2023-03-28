c7fa487d555919b85254b5b295bf24c420e55022
Shadowlight001 <taifjihane533@gmail.com>
2023-03-28 12:51:49 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -6,8 +6,27 @@
 	 *
 	 * Return: Always 0.
 	 */
-	int main(void)
-	{
-	    print_alphabet_*10();
-	    return (0);
-	}
+int main(void)
+		
+{
+		
+    int r;
+		
+
+		
+    r = _islower('H');
+		
+    _putchar(r + '0');
+		
+    r = _islower('o');
+		
+    _putchar(r + '0');
+		
+    r = _islower(108);
+		
+    _putchar(r + '0');
+		
+    _putchar('\n');
+		
+    return (0);
+}
